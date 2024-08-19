ddbebc63eaa8aa7f9e19f677f97f2c01ebd9c365
Christian <christianrh2000@hotmail.com>
2024-08-19 11:04:00 -0300

    2024-08-19 11:03

--- gui/company.py
+++ gui/company.py
@@ -92,10 +92,13 @@ class CompanyWindow:
         self.root = ctk.CTkToplevel()
         self.root.title('Empresas')
         self.root.grab_set()
-        self.root.config(padx=10, pady=10)
+        self.root.config(padx=4, pady=4)
 
     def agregar_widget(self, widget):
         widget.pack()
+
+    def agregar_widget2(self, widget):
+        widget.grid()
 
     def cerrar_ventana(self):
         self.root.winfo_parent()
@@ -242,54 +245,66 @@ class CompanyWidgets:
                              border_color="black",
                              )
 
+        marco.grid_rowconfigure(0, weight=1)
+        marco.grid_columnconfigure(0, weight=1)
         # CUIT
-        companyCUIT_label = ctk.CTkLabel(marco, text="CUIT",).place(x=10, y=10)
+        companyCUIT_label = ctk.CTkLabel(marco, text="CUIT", ).place(x=10, y=10)
         companyCUIT_entry = ctk.CTkEntry(marco, textvariable=datos['cuit'], width=100).place(x=115, y=10)
         # Razón Social
         companyRZ_label = ctk.CTkLabel(marco, text="Razón Social").place(x=10, y=40)
         companyRZ_entry = ctk.CTkEntry(marco, textvariable=datos['company_name'], width=300).place(x=115, y=40)
         # Nombre de Fantasia
-        companyNF_label = ctk.CTkLabel(marco, text="Nombre Fantasia",).place(x=10, y=70)
+        companyNF_label = ctk.CTkLabel(marco, text="Nombre Fantasia", ).place(x=10, y=70)
         companyNF_entry = ctk.CTkEntry(marco, textvariable=datos['fantasy_name'], width=300).place(x=115, y=70)
         # Dirección
-        companyDIR_label = ctk.CTkLabel(marco, text="Dirección",).place(x=10, y=100)
+        companyDIR_label = ctk.CTkLabel(marco, text="Dirección", ).place(x=10, y=100)
         companyDIR_entry = ctk.CTkEntry(marco, textvariable=datos['address'], width=220).place(x=115, y=100)
         # Teléfono
-        companyTEL_label = ctk.CTkLabel(marco, text="Teléfono",).place(x=350, y=100)
+        companyTEL_label = ctk.CTkLabel(marco, text="Teléfono", ).place(x=350, y=100)
         companyTEL_entry = ctk.CTkEntry(marco, textvariable=datos['phone'], width=100).place(x=415, y=100)
         # Número de Dependencia DGI-AFIP
-        companyNDA_label = ctk.CTkLabel(marco, text="Dep. AFIP/DGI",).place(x=10, y=130)
+        companyNDA_label = ctk.CTkLabel(marco, text="Dep. AFIP/DGI", ).place(x=10, y=130)
         companyNDA_entry = ctk.CTkEntry(marco, textvariable=datos['dependency_afip'], width=40).place(x=115, y=130)
         # Código de Actividad
-        companyCODA_label = ctk.CTkLabel(marco, text="Cód. Actividad",).place(x=10, y=160)
+        companyCODA_label = ctk.CTkLabel(marco, text="Cód. Actividad", ).place(x=10, y=160)
         companyCODA_entry = ctk.CTkEntry(marco, textvariable=datos['activity_code'], width=60).place(x=115, y=160)
-        companyCODAD_label = ctk.CTkLabel(marco, text="...",).place(x=185, y=160)
+        companyCODAD_label = ctk.CTkLabel(marco, text="...", ).place(x=185, y=160)
         # Condición ante el IVA
-        companyIVA_label = ctk.CTkLabel(marco, text="Cond. IVA",).place(x=10, y=190)
+        companyIVA_label = ctk.CTkLabel(marco, text="Cond. IVA", ).place(x=10, y=190)
         companyIVA_entry = ctk.CTkEntry(marco, textvariable=datos['iva_conditions'], width=40).place(x=115, y=190)
         companyIVAD_label = ctk.CTkLabel(marco, text="...", ).place(x=165, y=190)
         # El que Suscribe...
-        companySUSCR_label = ctk.CTkLabel(marco, text="El que Suscribe",).place(x=10, y=220)
+        companySUSCR_label = ctk.CTkLabel(marco, text="El que Suscribe", ).place(x=10, y=220)
         companySUSCR_entry = ctk.CTkEntry(marco, textvariable=datos['undersigned'], width=300).place(x=115, y=220)
         # En su Carácter de ...
-        companySUSCC_label = ctk.CTkLabel(marco, text="Carácter",).place(x=10, y=250)
-        companySUSCC_entry = ctk.CTkEntry(marco, textvariable=datos['undersigned_character'], width=200).place(x=115, y=250)
+        companySUSCC_label = ctk.CTkLabel(marco, text="Carácter", ).place(x=10, y=250)
+        companySUSCC_entry = ctk.CTkEntry(marco, textvariable=datos['undersigned_character'], width=200).place(x=115,
+                                                                                                               y=250)
 
+        marco_btns = ctk.CTkFrame(master=self.ventana_principal.root,
+                                  width=620,
+                                  height=420,
+                                  corner_radius=0,
+                                  fg_color='transparent'
+                                  )
 
-
-        clear_btn = ctk.CTkButton(marco, text="Vaciar", width=80,
+        clear_btn = ctk.CTkButton(marco_btns, text="Vaciar", width=80,
+                                  fg_color='transparent',
                                   command=lambda: limpiar_form(marco))
-        cancel_btn = ctk.CTkButton(marco, text="Cancelar", width=80,
+        cancel_btn = ctk.CTkButton(marco_btns, text="Cancelar", width=80,
                                    command=lambda: self.ventana_principal.cerrar_ventana())
-        ok_btn = ctk.CTkButton(marco, text="Guardar", width=120,
+        ok_btn = ctk.CTkButton(marco_btns, text="Guardar", width=120,
+                               fg_color='green',
                                command=lambda: validation_form(self, datos, opt)
                                )
 
-        clear_btn.place(x=12, y=370)
-        cancel_btn.place(x=205, y=370)
-        ok_btn.place(x=290, y=370)
+        clear_btn.grid(row=0, column=0, columnspan=2,  padx=15, pady=5, )
+        cancel_btn.grid(row=0, column=2, padx=15, pady=5, )
+        ok_btn.grid(row=0, column=3, padx=15, pady=5, )
 
-        self.ventana_principal.agregar_widget(marco)
+        print(marco.grid_slaves())
+        self.ventana_principal.agregar_widget2(marco)
+        self.ventana_principal.agregar_widget2(marco_btns)
 
         def validation_form(self, dataForm, optt):
 
@@ -394,8 +409,8 @@ def create_window():
     crear_widgets = CompanyWidgets(ventana_principal)
     return crear_widgets
 
-def company(opt=None, ventana_principal=None):
 
+def company(opt=None, ventana_principal=None):
     match opt:
         case "new":
             # Cerrar la ventana actual
